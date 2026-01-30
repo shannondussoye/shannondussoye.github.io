b1b127cca5524b6bd25e9169922d6605126dc572
Shannon Dussoye <shannon@MacBookAir.lan>
2026-01-31 00:23:27 +1100

    Fix TypeScript errors and unused variable warnings

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,7 +6,7 @@ export const BLOG_PATH = "src/data/blog";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: `./${BLOG_PATH}` }),
-  schema: ({ image }) =>
+  schema: () =>
     z.object({
       author: z.string().default(SITE.author),
       pubDatetime: z.coerce.date(),
